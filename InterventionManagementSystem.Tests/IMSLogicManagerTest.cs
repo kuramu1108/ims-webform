83c70dd21e7824ad762670bdc87e05a78d88db69
Po <colagarychen@gmail.com>
2017-04-02 15:48:44 +1000

    manager unit test class init

--- InterventionManagementSystem.Tests/IMSLogicManagerTest.cs
+++ InterventionManagementSystem.Tests/IMSLogicManagerTest.cs
@@ -14,5 +14,35 @@ namespace InterventionManagementSystem.Tests
         {
             manager = new Manager(2, "Luke", "luke1234", Support.convertToSS("12345678"), Common.UserType.Manager, Common.Districts.Sydney, 10, 500);
         }
+
+        [TestMethod]
+        public void IMSLogicManager_ChangePassword_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicManager_ChangePassword_SamePassword_Failed()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicManager_ViewListofInterventionCanApprove()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicManager_ApproveIntervention_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicManager_ApproveIntervention_Failed()
+        {
+            
+        }
     }
 }
