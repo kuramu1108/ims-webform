6c7ef61d687ba5136f2529dfca2dda77cf7b920a
Po <colagarychen@gmail.com>
2017-04-02 15:48:25 +1000

    accountant unit test

--- InterventionManagementSystem.Tests/IMSLogicAccountantTest.cs
+++ InterventionManagementSystem.Tests/IMSLogicAccountantTest.cs
@@ -15,7 +15,40 @@ namespace InterventionManagementSystem.Tests
             accountant = new Accountant(1, "po-hao chen", "pohao1108", Support.convertToSS("12345678"), Common.UserType.Accountant);
         }
 
-        //[TestMethod]
-        //public 
+        [TestMethod]
+        public void IMSLogicAccountant_ChangePassword_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicAccountant_ChangePassword_SamePassword_Fail()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicAccountant_GenerateReport_Success()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicAccountant_ChangeDistrict_SiteEngineer()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicAccountant_ChangeDistrict_Manager()
+        {
+
+        }
+
+        [TestMethod]
+        public void IMSLogicAccountant_ViewListofSiteEngineerManager()
+        {
+
+        }
     }
 }
