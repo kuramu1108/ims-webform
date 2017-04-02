fc8b288910dfd83c0a1ad3dd17f3ea766a82c978
Po <colagarychen@gmail.com>
2017-04-02 14:48:24 +1000

    initialize IMSLogicUserTest

---
+++ InterventionManagementSystem.Tests/IMSLogicUserTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMSLogic;
+using System.Security;
+
+namespace InterventionManagementSystem.Tests
+{
+    [TestClass]
+    public class IMSLogicUserTest
+    {
+        private Accountant accountant;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            accountant = new Accountant(1, "po-hao chen", "pohao1108", new SecureString(), Common.UserType.Accountant);
+
+        }
+
+        [TestMethod]
+        public void IMSLogicUser_()
+        {
+
+        }
+    }
+
+
+}
