37824b264f2132f3d47d699911efa146916c652c
yuan_chai <chai1125760921@hotmail.com>
2017-04-09 14:38:49 +1000

    UserDM

---
+++ IMSDBLayer/AccountantDM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSDBLayer
+{
+    public class AccountantDM : UsersDM
+    {
+
+        public  AccountantDM() : base() { }
+
+        public AccountantDM(int id, string name, string loginName, SecureString password, Common.UserType type) : base() { }
+
+    }
+}
