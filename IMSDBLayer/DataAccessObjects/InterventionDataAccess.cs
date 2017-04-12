eb95d3c6b458f8624b14ac9ddf1b37152f6c9fba
Alice Guo <12498996@student.uts.edu.au>
2017-04-12 19:56:03 +1000

    Intervention Read SQL Excution added

--- IMSDBLayer/DataAccessObjects/InterventionDataAccess.cs
+++ IMSDBLayer/DataAccessObjects/InterventionDataAccess.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using IMSDBLayer.DataAccessInterfaces;
 using IMSDBLayer.DataModels;
 using IMSDBLayer.Enums;
+using System.Data.SqlClient;
 
 namespace IMSDBLayer.DataAccessObjects
 {
@@ -25,6 +26,11 @@ namespace IMSDBLayer.DataAccessObjects
             throw new NotImplementedException();
         }
 
+        public bool update(Intervention intervention)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<Intervention> fetchInterventionsByCreator(Guid creatorId)
         {
             throw new NotImplementedException();
@@ -40,9 +46,55 @@ namespace IMSDBLayer.DataAccessObjects
             throw new NotImplementedException();
         }
 
-        public bool update(Intervention intervention)
+        private List<Intervention> executeReadSqlCommand(SqlCommand command)
         {
-            throw new NotImplementedException();
+            List<Intervention> interventions = new List<Intervention>();
+            using (SqlConnection connection = new SqlConnection(connstring))
+            {
+                command.Connection = connection;
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Intervention intervention = new Intervention();
+                    intervention.Id = reader.GetGuid(0);
+                    intervention.Hours = reader.GetDecimal(0);
+                    intervention.Costs = reader.GetDecimal(1);
+                    intervention.LifeRemaining = reader.GetInt32(2);
+                    intervention.Comments = reader.GetString(3);
+                    intervention.State = (InterventionState)reader.GetInt32(4);
+                    intervention.DateCreate = reader.GetDateTime(5);
+                    intervention.DateFinish = reader.GetDateTime(6);
+                    intervention.DateRecentVisit = reader.GetDateTime(7);
+                    intervention.InterventionTypeId = reader.GetGuid(8);
+                    intervention.ClientId = reader.GetGuid(9);
+                    intervention.CreatedBy = reader.GetGuid(10);
+                    intervention.ApprovedBy = reader.GetGuid(11);
+
+                    interventions.Add(intervention);
+                }
+                connection.Close();
+            }
+            return interventions;
+        }
+
+        private bool executeUpdateSqlCommand(SqlCommand command)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection())
+                {
+                    command.Connection = connection;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO - Implement low DB SQL excution exception logging.
+                return false;
+            }
+            return true;
         }
     }
 }
