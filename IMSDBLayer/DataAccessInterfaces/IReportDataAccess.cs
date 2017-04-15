4f8bfbccc1438f8fdb354d002150cc78790082b8
Yuan Chai <11998402@student.uts.edu.au>
2017-04-15 16:02:37 +1000

    Add report in dblayer(empty)

---
+++ IMSDBLayer/DataAccessInterfaces/IReportDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using IMSDBLayer.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSDBLayer.DataAccessInterfaces
+{
+    public interface IReportDataAccess
+    {
+        Report totalCostByEngineerReport();
+
+        Report averageCostByEngineerReport();
+
+        Report costByDistrictReport();
+
+        Report MonthlyCostByDistrict();
+
+    }
+}
