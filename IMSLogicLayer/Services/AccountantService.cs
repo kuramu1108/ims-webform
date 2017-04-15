29c9268e9b4c87440c3c4c27496c2538f774f869
Yuan Chai <11998402@student.uts.edu.au>
2017-04-15 11:56:40 +1000

    AccountantService

--- IMSLogicLayer/Services/AccountantService.cs
+++ IMSLogicLayer/Services/AccountantService.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using IMSLogicLayer.ServiceInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IMSDBLayer.DataModels;
+using IMSLogicLayer.Enums;
 
 namespace IMSLogicLayer.Services
 {
-    public class AccountantService
+    public class AccountantService : BaseService,IAccountantService
     {
+        private Guid accoutantId;
+        public AccountantService(string connstring) : base(connstring)
+        {
+        }
+
+        public IEnumerable<User> getAllManger()
+        {
+            return Users.fetchUsersByUserType((int)UserType.Manager);
+        }
+
+        public IEnumerable<User> getAllSiteEngineer()
+        {
+            return Users.fetchUsersByUserType((int)UserType.SiteEngineer);
+        }
+
+        public IEnumerable<User> getAllUser()
+        {
+           return Users.getAll();
+        }
+
+        public User getDetail()
+        {
+            return (User)Users.fetchUserByIdentityId(accoutantId);
+        }
+
+        public User getUserById(Guid userId)
+        {
+            return Users.fetchUserById(userId);
+        }
     }
 }
