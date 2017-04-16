58671bae7575d1e8f0d2cdcb0caba9963e909ecd
Po <colagarychen@gmail.com>
2017-04-16 15:55:52 +1000

    fake account service guid string exception fixed

--- IMSLogicLayer/FakeServices/FakeAccountantService.cs
+++ IMSLogicLayer/FakeServices/FakeAccountantService.cs
@@ -20,7 +20,7 @@ namespace IMSLogicLayer.FakeServices
             var ListOfManager = new List<User>()
             {
                 
-                new User("John Smith", 2,12m,2000.00m,new Guid("22222222-2222-2222-2222-222222222222"),new Guid("33333333-3333-3333-3333-3333-333333333333") ),
+                new User("John Smith", 2,12m,2000.00m,new Guid("22222222-2222-2222-2222-222222222222"),new Guid("33333333-3333-3333-3333-333333333333") ),
                 new User("Michael Jack",2,12m,2500.00m,new Guid("00000000-0000-0000-0000-000000000000"),new Guid("11111111-1111-1111-1111-111111111111"))
 
             };
@@ -34,7 +34,7 @@ namespace IMSLogicLayer.FakeServices
         {
             var ListOfEngineer = new List<User>()
             {
-                new User("John Miller", 1,6m,1000.00m,new Guid("44444444-4444-4444-4444-444444444444"),new Guid("66666666-6666-6666-6666-6666-666666666666") ),
+                new User("John Miller", 1,6m,1000.00m,new Guid("44444444-4444-4444-4444-444444444444"),new Guid("66666666-6666-6666-6666-666666666666") ),
                 new User("Jack James",1,6m,1500.00m,new Guid("55555555-5555-5555-5555-555555555555"),new Guid("77777777-7777-7777-7777-777777777777"))
 
             };
