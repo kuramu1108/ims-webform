15fd439b9daf7f05c93cdb31c5fa89b7512f33ae
Po <colagarychen@gmail.com>
2017-04-23 16:58:42 +1000

    interventionServiceTest completed

--- InterventionManagementSystem.Tests/IMSLogicInterventionServiceTest.cs
+++ InterventionManagementSystem.Tests/IMSLogicInterventionServiceTest.cs
@@ -2,11 +2,162 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using IMSDBLayer.DataAccessInterfaces;
+using IMSLogicLayer.Models;
+using IMSLogicLayer.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace InterventionManagementSystem.Tests
 {
+    [TestClass]
     class IMSLogicInterventionServiceTest
     {
+        private InterventionService interventionService;
+        private Intervention intervention;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Mock<IClientDataAccess> clients = new Mock<IClientDataAccess>();
+            Mock<IUserDataAccess> users = new Mock<IUserDataAccess>();
+            Mock<IDistrictDataAccess> districts = new Mock<IDistrictDataAccess>();
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            Mock<IInterventionTypeDataAccess> interventionTypes = new Mock<IInterventionTypeDataAccess>();
+
+            interventionService = new InterventionService("");
+
+            interventionService.Clients = clients.Object;
+            interventionService.Users = users.Object;
+            interventionService.Districts = districts.Object;
+            interventionService.Interventions = interventions.Object;
+            interventionService.InterventionTypes = interventionTypes.Object;
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_CompletedIntervention()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Completed, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Approved);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_CancelledIntervention()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Cancelled, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Approved);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_ApprovedIntervention_To_ProposedOrApproved()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Approved, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool toApprovedresult = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Approved);
+            bool toProposedresult = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Proposed);
+
+            Assert.IsFalse(toApprovedresult);
+            Assert.IsFalse(toProposedresult);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_ApprovedIntervention_To_CompletedOrCancelled()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Approved, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool toCompletedresult = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Completed);
+            bool toCancelledresult = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Cancelled);
+
+            Assert.IsTrue(toCompletedresult);
+            Assert.IsTrue(toCancelledresult);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_ProposedIntervention_To_Completed()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Proposed, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Completed);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionState_ProposedIntervention()
+        {
+            intervention = new Intervention(10, 4, 4, "", IMSLogicLayer.Enums.InterventionState.Proposed, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionState(intervention.Id, IMSLogicLayer.Enums.InterventionState.Approved);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionDetail()
+        {
+            string comment = "some comment";
+            int lifeRemaining = 50;
+            intervention = new Intervention(10, 4, lifeRemaining, comment, IMSLogicLayer.Enums.InterventionState.Approved, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventions.Setup(i => i.update(It.IsAny<Intervention>())).Returns(true);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionDetail(intervention.Id, comment, lifeRemaining);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionLastVisitDate()
+        {
+            intervention = new Intervention(10, 4, 40, "", IMSLogicLayer.Enums.InterventionState.Approved, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventions.Setup(i => i.update(It.IsAny<Intervention>())).Returns(true);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateInterventionLastVisitDate(intervention.Id, new DateTime());
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IMSLogicIntervention_UpdateInterventionApprovedBy()
+        {
+            intervention = new Intervention(10, 4, 40, "", IMSLogicLayer.Enums.InterventionState.Approved, new DateTime(), new DateTime(), new DateTime(), new Guid(), new Guid(), new Guid(), new Guid());
+            Mock<IInterventionDataAccess> interventions = new Mock<IInterventionDataAccess>();
+            interventions.Setup(i => i.fetchInterventionsById(It.IsAny<Guid>())).Returns(intervention);
+            interventions.Setup(i => i.update(It.IsAny<Intervention>())).Returns(true);
+            interventionService.Interventions = interventions.Object;
+
+            bool result = interventionService.updateIntervetionApprovedBy(intervention.Id, new User());
+
+            Assert.IsTrue(result);
+        }
     }
 }
