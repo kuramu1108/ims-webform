358f020a8add4f82fd91453e9905bc9d205c7d48
Yuan Chai <11998402@student.uts.edu.au>
2017-05-01 22:18:17 +1000

    Fix bug navigation on sitemaster

--- InterventionManagementSystem/Engineer/InterventionDetail.aspx.cs
+++ InterventionManagementSystem/Engineer/InterventionDetail.aspx.cs
@@ -92,7 +92,7 @@ namespace InterventionManagementSystem.Engineer
         /// <param name="e"></param>
         protected void Edit_Click(object sender, EventArgs e)
         {
-            Response.Redirect("~/Engineer/EditIntervention.aspx?id=" + intervention.Id,true);
+            Response.Redirect("~/Engineer/EditIntervention.aspx?id=" + Request.QueryString["Id"], true);
         }
         /// <summary>
         /// Redirect to change state page with query string to process state of an intervention
@@ -101,7 +101,7 @@ namespace InterventionManagementSystem.Engineer
         /// <param name="e"></param>
         protected void changeState_Click(object sender, EventArgs e)
         {
-            Response.Redirect("~/Engineer/ChangeState.aspx?id="+ intervention.Id,true);
+            Response.Redirect("~/Engineer/ChangeState.aspx?id="+ Request.QueryString["Id"], true);
 
         }
     }
